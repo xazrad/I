b3868ef7afcd7e639f0f6a13820fe6d932324df5
Khaziev Radik <xazrad@gmail.com>
2018-10-30 13:49:02 +0300

    first commit

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-mkdir -p /opt/infographika_web/static
-mkdir -p /opt/infographika_web/db
+mkdir -p /opt/infographika_web/static \
+    && mkdir -p /opt/infographika_web/db \
+    && apt-get install -y git-core \
+    && cd /opt/infographika_web \
+    && git clone git@github.com:xazrad/I.git . \
+    && docker-compose up -d --build
