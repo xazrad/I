f9cb89f3aecf26e2a612f9a766a3f9c9008bd905
Khaziev Radik <xazrad@gmail.com>
2018-10-30 13:50:52 +0300

    first commit

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 mkdir -p /opt/infographika_web/static \
     && mkdir -p /opt/infographika_web/db \
     && apt-get install -y git-core \
+    && apt-get install -y docker.io \
+    && curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+    && chmod +x /usr/local/bin/docker-compose \
+    && docker login registry.gitlab.com -u gitlab+deploy-token-24148 -p BEBV7qg4-mEzjYYHZEc1 \
     && cd /opt/infographika_web \
     && git clone git@github.com:xazrad/I.git . \
     && docker-compose up -d --build
